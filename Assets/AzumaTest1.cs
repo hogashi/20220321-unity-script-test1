d0a5e2a930d3793f009978c9f1db69cc0e8eb751
hogashi <azuma.ginkakuji@gmail.com>
2022-03-21 20:16:18 +0900

    add script

---
+++ Assets/AzumaTest1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AzumaTest1 : MonoBehaviour
+{
+  // Start is called before the first frame update
+  public string myName;
+
+  void Start()
+  {
+    BoxCollider boxCollider = GetComponent<BoxCollider>();
+    Debug.Log("I am alive! size: " + boxCollider.size);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    
+  }
+}
