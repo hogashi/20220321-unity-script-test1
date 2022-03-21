7625340ae68e047df2f82e11bf836672c4755850
hogashi <azuma.ginkakuji@gmail.com>
2022-03-21 20:32:59 +0900

    get another game object

--- Assets/AzumaTest1.cs
+++ Assets/AzumaTest1.cs
@@ -6,16 +6,18 @@ public class AzumaTest1 : MonoBehaviour
 {
   // Start is called before the first frame update
   public string myName;
+  public GameObject targetCube;
 
   void Start()
   {
     BoxCollider boxCollider = GetComponent<BoxCollider>();
     Debug.Log("I am alive! size: " + boxCollider.size);
+
+    Debug.Log("target position: " + targetCube.transform.position);
   }
 
   // Update is called once per frame
   void Update()
   {
-    
   }
 }
